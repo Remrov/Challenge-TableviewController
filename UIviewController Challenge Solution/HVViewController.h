475df238a313c4ccc96873683b5f900f179d21f5
Heidi Vormer <heidivormer@Heidis-MacBook-Air.local>
2013-10-21 07:54:03 -0400

    Initial Commit

---
+++ UIviewController Challenge Solution/HVViewController.h
@@ -0,0 +1,13 @@
+//
+//  HVViewController.h
+//  UIviewController Challenge Solution
+//
+//  Created by Heidi Vormer on 2013-10-21.
+//  Copyright (c) 2013 Heidi Vormer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HVViewController : UIViewController
+
+@end
---
+++ UIviewController Challenge SolutionTests/UIviewController_Challenge_SolutionTests.m
@@ -0,0 +1,34 @@
+//
+//  UIviewController_Challenge_SolutionTests.m
+//  UIviewController Challenge SolutionTests
+//
+//  Created by Heidi Vormer on 2013-10-21.
+//  Copyright (c) 2013 Heidi Vormer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIviewController_Challenge_SolutionTests : XCTestCase
+
+@end
+
+@implementation UIviewController_Challenge_SolutionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
